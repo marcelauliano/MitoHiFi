954971262de5919725e9310956cad121f6054fb1
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-08-19 16:01:19 +0100

    Update cut_coords.py

--- scripts/cut_coords.py
+++ scripts/cut_coords.py
@@ -5,7 +5,11 @@ circu_info = open('circucheck', 'r')
 file = circu_info.read()
 obj=file.split(",")
 if obj[1] == " True":
-    record=SeqIO.read(input_fasta, "fasta")
-    id = record.id
-    get= record[int(obj[3]):]
-    print(get.format('fasta'))
+        record=SeqIO.read(input_fasta, "fasta")
+        id = record.id
+        get= record[int(obj[3]):]
+        print(get.format('fasta'))
+else:
+        record=SeqIO.read(input_fasta, "fasta")
+        id = record.id  
+        print(record.format('fasta'))
