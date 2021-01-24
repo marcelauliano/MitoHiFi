808d89f263bf57348b8c52381592fea7daecb857
Bjoern Gruening <bjoern.gruening@gmail.com>
2021-01-24 21:47:11 +0100

    restructure the repo to be more package like

---
+++ src/mitohifi/get_Larger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Credit: Marcela Uliano-Silva
+
+import sys
+
+from Bio import SeqIO
+
+out1 = {}
+for sequence in SeqIO.parse(sys.argv[1], "fasta"):
+    id = sequence.id
+    length = len(sequence)
+    out1[id] = length
+sort_out1 = sorted(out1.items(), key=lambda x: x[1], reverse=True)
+largest = sort_out1[0]
+print(largest[0])
