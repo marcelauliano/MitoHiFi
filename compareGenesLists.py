4990b18c59e0ab3be87cfbdc91c51a5bc80850a9
Joao Ferreira <jf18@sanger.ac.uk>
2022-10-13 19:51:37 +0100

    Modularize script to compare list of genes

--- compareGenesLists.py
+++ compareGenesLists.py
@@ -28,10 +28,14 @@ def get_clean_gene(in_gene):
             gene_clean = "trn" + seq1(gene_clean[3:])
         else:
             gene_clean = "trn" + amino_code
+    elif in_gene.lower().startswith("nad"):
+        gene_clean = in_gene.lower().replace("nad", "nd")    
     else:
-            gene_clean = in_gene.replace("-", "").lower()
+        gene_clean = in_gene.replace("-", "").lower()
 
-def compare_genes_dicts(genes1, genes2):
+    return gene_clean
+
+def compare_genes_dicts(genes1, genes2, alphabetically_sorted=False):
     """
     Takes two lists of genes and return genes shared and specific.
     """
@@ -41,66 +45,68 @@ def compare_genes_dicts(genes1, genes2):
     
     genes1_counts_clean = {}
     for gene1, n in genes1_counts.items():
-        if gene1.lower().startswith("trn"):
-            gene1_clean = gene1.replace("tRNA-", "trn")
-            print(f"gene1_clean: {gene1_clean}")
-            amino_code = gene1_clean[3:]
-            if len(amino_code) == 3:
-                gene1_clean = "trn" + seq1(gene1_clean[3:])
-            else:
-                gene1_clean = "trn" + amino_code
-        else:
-            gene1_clean = gene1.replace("-", "").lower()
+        gene1_clean = get_clean_gene(gene1)
         genes1_counts_clean[gene1_clean] = n
     
     genes2_counts_clean = {}
     for gene2, n in genes2_counts.items():
-        if gene2.lower().startswith("trn"):
-            gene2_clean = gene2.replace("tRNA-", "trn")
-            amino_code = gene2_clean[3:]
-            if len(amino_code) == 3:
-                gene2_clean ="trn" + seq1(gene2_clean[3:])
-            else:
-                gene2_clean = "trn" + amino_code
-        else:
-            gene2_clean = gene2.replace("-", "").lower()
+        gene2_clean = get_clean_gene(gene2)
         genes2_counts_clean[gene2_clean] = n
     
-    #genes1_counts_clean = {k.replace("-", "").replace("tRNA", "trn").lower(): v for k, v in genes1_counts.items()}
-    #genes2_counts_clean = {k.replace("-", "").replace("tRNA", "trn").lower(): v for k, v in genes2_counts.items()}
-    print("### genes1_counts:")
-    print(genes1_counts)
-    print("### genes1_counts_clean:")
-    print(genes1_counts_clean)
-    print("### genes2_counts")
-    print(genes2_counts)
-    print("### genes2_counts_clean:")
-    print(genes2_counts_clean)
+   # print("### genes1_counts:")
+   # print(genes1_counts)
+   # print("### genes1_counts_clean:")
+   # print(genes1_counts_clean)
+   # print("### genes2_counts")
+   # print(genes2_counts)
+   # print("### genes2_counts_clean:")
+   # print(genes2_counts_clean)
     
     shared_genes = {}
     genes1_specific_genes = {}
     genes2_specific_genes = {}
 
     for gene1 in genes1_counts:
-        gene1_clean = gene1.replace("-", "").lower()
+        gene1_clean = get_clean_gene(gene1)
         if gene1_clean in genes2_counts_clean:
             shared_genes[gene1] = [genes1_counts[gene1], genes2_counts_clean[gene1_clean]]
         else:
             genes1_specific_genes[gene1] = [genes1_counts[gene1], 0]
     
     for gene2 in genes2_counts:
-        gene2_clean = gene2.replace("-", "").lower()
+        gene2_clean = get_clean_gene(gene2)
         if gene2_clean not in genes1_counts_clean:
             genes2_specific_genes[gene2] = [0, genes2_counts[gene2]]
-
-    print(f"shared genes: {shared_genes}")
-    print(f"specific to file1: {genes1_specific_genes}")
-    print(f"specific to file2: {genes2_specific_genes}")
+    
+    if alphabetically_sorted:
+        shared_genes_sorted = dict(sorted(shared_genes.items()))
+        genes1_specific_genes_sorted = dict(sorted(genes1_specific_genes.items()))
+        genes2_specific_genes_sorted = dict(sorted(genes2_specific_genes.items()))
+        return (shared_genes_sorted, genes1_specific_genes_sorted, genes2_specific_genes_sorted)
+    else:
+        return (shared_genes, genes1_specific_genes, genes2_specific_genes)
 
 if __name__ == "__main__":
-    genes_list1 = get_genes_list(sys.argv[1], sys.argv[2])
-    genes_list2 = get_genes_list(sys.argv[3], sys.argv[4])
-    #print(genes_list1)
-    #print(genes_list2)
-    compare_genes_dicts(genes_list1, genes_list2)
-    #print(get_genes_counts(genes_list))
+    if sys.argv[1] in ["-h", "-help", "--help"]:
+        print("""Run usage:
+        python compare <annotationFile1> <file1Format>[genbank|gff] <annotationFile2> <file2Format>[genbank|gff] <returnGenesInAlphabeticalOrder>[True|False]\n""")
+        sys.exit(0)
+
+    annotation1 = sys.argv[1]
+    annotation1_format = sys.argv[2]
+    annotation2 = sys.argv[3]
+    annotation2_format = sys.argv[4]
+    alphabetically_sorted_string = sys.argv[5]
+    if alphabetically_sorted_string == "True":
+        alphabetically_sorted = True
+    elif alphabetically_sorted_string == "False":
+        alphabetically_sorted = False
+    else:
+        print("alphabetically_sorted argument must be either True or False")
+        sys.exit(1)
+    genes_list1 = get_genes_list(annotation1, annotation1_format)
+    genes_list2 = get_genes_list(annotation2, annotation2_format)
+    shared, annotation1_specific, annotation2_specific = compare_genes_dicts(genes_list1, genes_list2, alphabetically_sorted)
+    print(f"Shared genes:\n{shared}\n\n")
+    print(f"Specific to {annotation1}:\n{annotation1_specific}\n\n")
+    print(f"Specific to {annotation2}:\n{annotation2_specific}\n\n")
