ea69d6ba5cca3fa496bef852433194ff1ff4965e
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2022-12-20 14:51:33 -0300

    Update mitos_wrapper.sh

--- mitos_wrapper.sh
+++ mitos_wrapper.sh
@@ -2,7 +2,7 @@
 
 (
 eval "$($CONDA_DIR/bin/conda shell.bash hook)"
-conda activate mitofinder &&
+conda activate mitos_env &&
 PYTHONPATH= /home/mu/miniconda3/envs/mitos_env/bin/runmitos.py "$@"
 conda deactivate
 )
