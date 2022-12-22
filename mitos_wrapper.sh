a6cfcfb0427a9d222d5dfe80332a0db8300353b3
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2022-12-22 14:38:10 -0300

    Update mitos_wrapper.sh

--- mitos_wrapper.sh
+++ mitos_wrapper.sh
@@ -3,6 +3,6 @@
 (
 eval "$($CONDA_DIR/bin/conda shell.bash hook)"
 conda activate mitos_env &&
-PYTHONPATH= /home/mu/miniconda3/envs/mitos_env/bin/runmitos.py "$@"
+runmitos.py "$@"
 conda deactivate
 )
